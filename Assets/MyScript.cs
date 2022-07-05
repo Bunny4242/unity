7d54257d3db2aa751396feedb7a7eddab8006b6d
Bunny4242 <pannipa.ko.64@ubu.ac.th>
2022-07-05 10:48:54 +0700

    Update MyScript.cs

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -6,7 +6,11 @@ public class MyScript : MonoBehaviour
 {
     void Start()
     {
-        Debug.Log("Hello World");
+        //Debug.Log("Hello World");
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+        Debug.Log(z);
     }
 
  

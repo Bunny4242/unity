948205332634b6f569c8e2cfa8010561c500094c
Bunny4242 <pannipa.ko.64@ubu.ac.th>
2022-07-05 16:10:17 +0700

    two1

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -1,16 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Gameplay : MonoBehaviour
 {
-   
+    int T = 20, A = 20, I = 10, sum = 0;
+    public TextMeshProUGUI txtDisplay;
+    public TextMeshProUGUI txtDisplay1;
+    public TextMeshProUGUI txtDisplay2;
+    public float timeRemaining = 20;
+
 
     // Update is called once per frame
     void Update()
     {
+        if (timeRemaining > 0 )
+        {
+            timeRemaining -= Time.deltaTime;
+            txtDisplay2.text = "Time : " + timeRemaining.ToString("0");
+
+        }
+        else if (sum <= 0 )
+        {
+            txtDisplay1.text = "Mission Failed";
+        }
+ 
         if (Input.GetMouseButtonDown(0))
         {
+           
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
@@ -18,16 +36,31 @@ public class Gameplay : MonoBehaviour
                 if (hit.collider.gameObject.name.CompareTo("RedSphere") == 0)
                 {
                     hit.collider.gameObject.SetActive(false);
+                    sum = sum - T;
                 }
 
-                if (hit.collider.gameObject.name.CompareTo("GreenSphere") == 1)
+                if (hit.collider.gameObject.name.CompareTo("GreenSphere") == 0)
                 { 
                     hit.collider.gameObject.SetActive(false);
+                    sum = sum + A;
                 }
 
-                if (hit.collider.gameObject.name.CompareTo("BlueSphere") == 1)
+                if (hit.collider.gameObject.name.CompareTo("BlueSphere") == 0)
                 {
                     hit.collider.gameObject.SetActive(false);
+                    sum = sum + I;
+                }
+                Debug.Log(sum);
+                txtDisplay.text = "Point: " + sum.ToString();
+                if(sum >= 120)
+                {
+                    txtDisplay1.text = "Mission Completed";
+                    timeRemaining = 0;
+                }
+                else if (sum<=0)
+                {
+                    txtDisplay1.text = "Mission Failed";
+                    timeRemaining = 0;
                 }
 
             }
